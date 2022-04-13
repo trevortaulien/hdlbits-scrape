7d65ca5a8ab4e1b125dc6919545b4c429cd79367
trevortaulien <51167085+trevortaulien@users.noreply.github.com>
2022-04-13 09:23:02 -0400

    w/ page source can get html w/ code and pretty it

--- selenium test.py
+++ selenium test.py
@@ -4,14 +4,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+from bs4 import BeautifulSoup
 
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 driver.get("https://hdlbits.01xz.net/wiki/Vector100r")
 driver.implicitly_wait(1)
 line = driver.find_element(By.CLASS_NAME, "CodeMirror-line")
+
 # print(driver.page_source)
+with open("seleniumPageSourceTest.html", "w") as s:
+    s.writelines(driver.page_source)
+
 driver.close()
+
+with open("seleniumPageSourceTest.html", "r") as r:
+    website = BeautifulSoup(r,"html.parser")
+
+with open("seleniumPageSourcePretty.txt","w") as p:
+    p.writelines(website.prettify())
 
 ###
 ### CodeMirror-line appears to be the class name that contains the information for each line of code
