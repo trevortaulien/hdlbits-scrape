3fee33a7691e91a7ca89465db56c39366e83b187
trevortaulien <51167085+trevortaulien@users.noreply.github.com>
2022-04-14 18:29:45 -0400

    file restructure & comments  explaining function

--- Selenium Testing/selenium test.py
+++ Selenium Testing/selenium test.py
@@ -1,5 +1,15 @@
 print("I'm running :)")
 
+#############################################################################################################
+# The purpose of this .py is to get the html from hdlbits as it is seen from the Chrome inspector           #
+# this is necessary because hdlbits uses CodeMirror which is a javascript text editor that the user         #
+# types verilog into. When using beautifulsoup to gather the html the only thing returned is CodeMirror.js  #
+# Selenium returns the html as seen in the Chrome inspector.                                                #
+# This .py file gets the html as seen in Chrome inspector using selenium then opens the html as a           #
+# beautiful soup object to be prettified because selenium returns a lot of the html inline, and so that     #
+# further operations can be preformed using beautifulsoup.                                                  #
+#############################################################################################################
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
@@ -10,18 +20,16 @@ driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 driver.get("https://hdlbits.01xz.net/wiki/Vector100r")
 driver.implicitly_wait(1)
-line = driver.find_element(By.CLASS_NAME, "CodeMirror-line")
 
-# print(driver.page_source)
-with open("seleniumPageSourceTest.html", "w") as s:
+with open("Selenium Testing/seleniumPageSourceTest.html", "w") as s:
     s.writelines(driver.page_source)
 
 driver.close()
 
-with open("seleniumPageSourceTest.html", "r") as r:
+with open("Selenium Testing/seleniumPageSourceTest.html", "r") as r:
     website = BeautifulSoup(r,"html.parser")
 
-with open("seleniumPageSourcePretty.txt","w") as p:
+with open("Selenium Testing/seleniumPageSourcePretty.txt","w") as p:
     p.writelines(website.prettify())
 
 ###
