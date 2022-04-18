5d523280cf3d41d2bfb9fd074a0b0c37196c1c68
trevortaulien <51167085+trevortaulien@users.noreply.github.com>
2022-04-18 13:41:57 -0400

    I can now go to each problem page one by one

--- problem-link-collection.py
+++ problem-link-collection.py
@@ -27,14 +27,26 @@ def login(driver):
 def go_to_stats(driver):
     name_dropdown = driver.find_element(By.XPATH,'/html/body/div[1]/nav/section/ul[1]/li[4]/a') 
     stats = driver.find_element(By.XPATH,'/html/body/div[1]/nav/section/ul[1]/li[4]/ul/li[4]/a')
-    
+
     action = ActionChains(driver)
     action.move_to_element(name_dropdown).perform()
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/nav/section/ul[1]/li[4]/ul/li[4]/a')))
     action.move_to_element(stats).click().perform()
 
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="firstHeading"]')))
-  
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[3]/div/div/div[2]/div[1]/table/tbody/tr[1]/td[1]/a')))
+
+def travel_to_each_problem(driver):
+    links = driver.find_elements(By.CLASS_NAME,"vlgstat_link")
+    action = ActionChains(driver)
+
+    for link in links:
+        link.location_once_scrolled_into_view
+        action.move_to_element(link).click().perform()
+        WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'hb-box')))
+        print(link)    
+        driver.back()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[3]/div/div/div[2]/div[1]/table/tbody/tr[1]/td[1]/a')))
+
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 driver.get("https://hdlbits.01xz.net/wiki/Special:VlgLogin")
@@ -42,6 +54,8 @@ driver.get("https://hdlbits.01xz.net/wiki/Special:VlgLogin")
 login(driver)
 
 go_to_stats(driver)
+
+travel_to_each_problem(driver)
 
 # driver.close()
  
