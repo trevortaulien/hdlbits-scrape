722e1fc9fdac08aa9d9da1c89f7f1c36ae6fb7da
trevortaulien <51167085+trevortaulien@users.noreply.github.com>
2022-04-19 14:43:03 -0400

    can write extracted HDL to file formatted properly

---
+++ Selenium Testing/WriteHDL-toFileTest.v
@@ -0,0 +1,7 @@
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+​
+endmodule
+​
--- page_source-code extraction text.py
+++ page_source-code extraction text.py
@@ -2,16 +2,31 @@ print("I'm running :)")
 
 from bs4 import BeautifulSoup
 
-with open("seleniumPageSourcePretty.txt", "r") as w:
+with open("Selenium Testing/seleniumPageSourcePretty.txt", "r") as w:
     website = BeautifulSoup(w, "html.parser")
 
 #CodeMirrorLines = website.select(".CodeMirror-line")
 
 CodeMirrorLines = website.find_all("pre", class_="CodeMirror-line")
 
+line_list = []
+
 for line in CodeMirrorLines:
     for text in line.next_element.contents:
         print(text.string, end="")
+        line_list.append(text.string)
     print()
+    line_list.append("\n")
+
+print(line_list)
+
+file_name = website.find("h2").string
+print(file_name)
+file_name_stripped = file_name.strip()
+print(file_name_stripped)
+
+with open("Selenium Testing/" + file_name.strip() + ".v", "w") as t:
+    for element in line_list:
+        t.write(element)
 
 print("\nI'm done :)")
